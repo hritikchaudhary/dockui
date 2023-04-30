2d00989e4cbc64e2286726c70fb4ee2933c71d31
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-05-01 04:49:31 +0530

    added docker-java and db setup done

--- openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
+++ openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
@@ -1,16 +1,41 @@
 package ai.openfabric.api.controller;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.exception.DockerException;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.util.List;
 
 @RestController
 @RequestMapping("${node.api.path}/worker")
 public class WorkerController {
 
+    DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+            .withDockerHost("unix:///var/run/docker.sock")
+            .build();
+    DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+            .dockerHost(config.getDockerHost())
+            .sslConfig(config.getSSLConfig())
+            .maxConnections(100)
+            .connectionTimeout(Duration.ofSeconds(30))
+            .responseTimeout(Duration.ofSeconds(45))
+            .build();
+    DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
 
     @PostMapping(path = "/hello")
     public @ResponseBody String hello(@RequestBody String name) {
         return "Hello!" + name;
     }
 
-
+    @GetMapping(path = "/containers")
+    public List<Container> getContainers() throws DockerException, InterruptedException {
+        return dockerClient.listContainersCmd().withShowAll(true).exec();
+    }
 }
